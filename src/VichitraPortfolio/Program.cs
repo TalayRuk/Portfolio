4d229117ada414d7918e4d4ef7d3bea7333b8900
vp <notejde@gmail.com>
2016-12-18 03:09:08 -0800

    add change client auser to empty string , and token

--- src/VichitraPortfolio/Program.cs
+++ src/VichitraPortfolio/Program.cs
@@ -16,19 +16,19 @@ namespace VichitraPortfolio
             //Make a connection with the server where the API is located 
             var client = new RestClient("https://api.github.com/search/repositories?page=1&q=user:talayruk&sort=stars&order=desc");
             //Create the request, and add the physical path to the specific API controller an choose the HTTP method
-            var request = new RestRequest("Client_ID/aa0091094cf01ddc2acebef180d37ef928bbe616/Repos", Method.GET);
+            var request = new RestRequest("", Method.GET);
             //Add parameters to our request. We have to set repository
+            request.AddParameter("Access_token", "81fb8907e791f232977c6c3ba867e1d1cd8b2c09");
             request.AddHeader("User-Agent", "talayruk");
             //To get metadata in search results, specify the text-match media type in Accept header
             request.AddHeader("Accept", "application/vnd.github.v3.text-match+json");
-            request.AddParameter("Access_token", "81fb8907e791f232977c6c3ba867e1d1cd8b2c09");
             //request.AddParameter("q=user", "talayruk");
             //request.AddParameter("Sort", "stars");
             //request.AddParameter("Order", "desc");
             //request.AddParameter("Number", "3");
 
             //Give the  client appropriate credentials
-            client.Authenticator = new HttpBasicAuthenticator("2d2756c3dd0a7e9a23c7", "aa0091094cf01ddc2acebef180d37ef928bbe616");
+            client.Authenticator = new HttpBasicAuthenticator("", "81fb8907e791f232977c6c3ba867e1d1cd8b2c09");
             //We initialize a new RestResponse variable named respons
             var response = new RestResponse();
             //The request ismade w/an asynchronous method, allows us to await asynchronous calls in a "synchronous" way. We set response = to the response from our request, which we make in the method 1) and then cast as the type REstresponse
