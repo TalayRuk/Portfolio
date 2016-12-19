87164d0f54b6471c0116edc4d839dd3b8c07fe62
vp <notejde@gmail.com>
2016-12-18 21:00:52 -0800

    add delete system.web fr default.cs  no longer available for dotnetcore

--- src/VichitraPortfolio/Models/Default.aspx.cs
+++ src/VichitraPortfolio/Models/Default.aspx.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class _Default : System.Web.UI.Page
+public partial class _Default 
 {
     protected void Page_Load(object sender, EventArgs e)
     {
